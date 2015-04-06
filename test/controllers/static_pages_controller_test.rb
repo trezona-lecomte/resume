c7cb9d2a2ba906f535ff46531cb2f9fdcdb9e88c
trezona-lecomte <trezona.lecomte@gmail.com>
2015-04-07 01:12:50 +1200

    Add custom page titles

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,13 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Hey, Powershop!"
   end
 
   test "should get resume" do
     get :resume
     assert_response :success
+    assert_select "title", "Resume - Kieran Trezona-le Comte"
   end
 
 end
