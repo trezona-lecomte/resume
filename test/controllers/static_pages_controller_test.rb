37a1701b9750c81d4182f36fbbc537e06650605a
trezona-lecomte <trezona.lecomte@gmail.com>
2015-04-08 20:05:58 +1200

    Add tests for all static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,16 +1,22 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
-  test "should get home" do
-    get :home
+  test "should get cover letter" do
+    get :cover_letter
     assert_response :success
-    assert_select "title", "Hey, Powershop!"
+    assert_select "title", "Cover Letter - Kieran Trezona-le Comte"
   end
 
   test "should get resume" do
     get :resume
     assert_response :success
     assert_select "title", "Resume - Kieran Trezona-le Comte"
+  end
+
+  test "should get puzzle solution" do
+    get :puzzle_solution
+    assert_response :success
+    assert_select "title", "Puzzle Solution - Kieran Trezona-le Comte"
   end
 
 end
